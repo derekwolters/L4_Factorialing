9283775990ae373e622f6ef9999061e0f952c4f0
Derek Wolters <derek.wolters@gmail.com>
2017-03-29 15:50:33 -0400

    inital commit

---
+++ L4_Factorialing/Factorialing.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace L4_Factorialing
+{
+    class Factorialing
+    {
+        static void Main(string[] args)
+        {
+            //initialize variables
+            bool keepGoing = true;
+
+            Console.WriteLine("This program will give the factorial a given " +
+                "number");
+
+            while (keepGoing)
+            {
+                //get input, calculate and display the results
+                Console.WriteLine(CalcFactorial(GetInput()));
+
+                //exit program               
+                if (ExitProgram()) break;
+            }
+        }
+        //get the user's number
+        public static int GetInput()
+        {
+            string input = "";
+            int temp;
+
+            Console.WriteLine("Enter a whole number from 1 to 10:");
+            input = Console.ReadLine();
+
+            if (!int.TryParse(input, out temp))
+            {
+                //check that input is an integer & ask for reentry if not
+                Console.WriteLine("Input should be a whole number. " +
+                    "Try again.");
+                return GetInput();
+            }
+            else if (temp < 1 || temp > 10)
+            {
+                //check that input is withing range & ask for reentry if not
+                Console.WriteLine("Input should be greater than 1 or less " +
+                     " than 10. Try again.");
+                return GetInput();
+            }
+            else { return temp; }
+        }
+
+        //create and display a table of calculated results
+        public static int CalcFactorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+
+            return num * CalcFactorial(num - 1);
+        }
+
+        //calculate the square of a number
+        public static int CalcSq(int num)
+        {
+            //Console.WriteLine(num * num);
+            return num * num;
+        }
+
+        //calculate the cube of a number
+        public static int CalcCu(int num)
+        {
+            //Console.WriteLine(num * num * num);
+            return num * num * num;
+        }
+
+        //exit the program when the user wants to
+        public static Boolean ExitProgram()
+        {
+            string xitChoice = "";
+
+            Console.WriteLine("Do you want to continue? Enter Y or N.");
+
+            xitChoice = Console.ReadLine().ToUpper();
+
+            while (xitChoice != "N" && xitChoice != "Y")
+            {
+                Console.WriteLine("Not a vaid answer. Do you want to " +
+                    "continue? Enter Y or N.");
+                xitChoice = Console.ReadLine().ToUpper();
+            }
+
+            return xitChoice == "N" ? true : false;
+        }
+    }
+}
